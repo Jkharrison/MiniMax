f85fa3c07efb639db564abdd8d3f511c55ff7a3e
Jasper Harrison <jasperharison@gmail.com>
2018-09-25 14:25:34 -0500

    Board object complete

--- src/Board.java
+++ src/Board.java
@@ -2,8 +2,8 @@ import java.util.Arrays;
 class Board
 {
     public char[][] board = {{'_','_','_'}, {'_', '_', '_'}, {'_', '_', '_'}};
-    private boolean xHasWon = false;
-    private boolean oHasWon = false;
+    // private boolean xHasWon = false;
+    // private boolean oHasWon = false;
     Board()
     {
 
@@ -20,34 +20,43 @@ class Board
     {
         char[] rowOfX = {'X', 'X', 'X'};
         if(Arrays.equals(rowOfX, this.board[0]))
-        {
             return true;
-        }
         else if(Arrays.equals(rowOfX, this.board[1]))
-        {
             return true;
-        }
         else if(Arrays.equals(rowOfX, this.board[2]))
-        {
             return true;
-        }
+        else if(this.board[0][0] == 'X' && this.board[1][1] == 'X' && this.board[2][2] == 'X')
+            return true;
+        else if(this.board[2][0] == 'X' && this.board[1][1] == 'X' && this.board[0][2] == 'X')
+            return true;
+        else if(this.board[0][0] == 'X' && this.board[1][0] == 'X' && this.board[2][0] == 'X')
+            return true;
+        else if(this.board[0][1] == 'X' && this.board[1][1] == 'X' && this.board[2][1] == 'X')
+            return true;
+        else if(this.board[0][2] == 'X' && this.board[1][2] == 'X' && this.board[2][2] == 'X')
+            return true;
+        return false;
         // TODO: Add in the rest of the winning conditions below.
     }
     public boolean oWon()
     {
         char[] rowOfO = {'O', 'O', 'O'};
         if(Arrays.equals(rowOfO, this.board[0]))
-        {
             return true;
-        }
         else if(Arrays.equals(rowOfO, this.board[1]))
-        {
             return true;
-        }
         else if(Arrays.equals(rowOfO, this.board[2]))
-        {
             return true;
-        }
+        else if(this.board[0][0] == 'O' && this.board[1][1] == 'O' && this.board[2][2] == 'O')
+            return true;
+        else if(this.board[2][0] == 'O' && this.board[1][1] == 'O' && this.board[0][2] == 'O')
+            return true;
+        else if(this.board[0][0] == 'O' && this.board[1][0] == 'O' && this.board[2][0] == 'O')
+            return true;
+        else if(this.board[0][1] == 'O' && this.board[1][1] == 'O' && this.board[2][1] == 'O')
+            return true;
+        else if(this.board[0][2] == 'O' && this.board[1][2] == 'O' && this.board[2][2] == 'O')
+                return true;
         // TODO: Add in the rest of the winning conditions below.
     }
 }
