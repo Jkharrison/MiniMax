1fc5b70fbb65f02add5f527aa1754cf87745db1b
Jasper Harrison <jasperharison@gmail.com>
2018-09-25 14:17:12 -0500

    Add the rest of the winning conditions and the board class will be complete

--- src/Board.java
+++ src/Board.java
@@ -1,3 +1,4 @@
+import java.util.Arrays;
 class Board
 {
     public char[][] board = {{'_','_','_'}, {'_', '_', '_'}, {'_', '_', '_'}};
@@ -15,8 +16,38 @@ class Board
     {
         this.board = that;
     }
-    void resetBoard()
+    public boolean xWon()
     {
-
+        char[] rowOfX = {'X', 'X', 'X'};
+        if(Arrays.equals(rowOfX, this.board[0]))
+        {
+            return true;
+        }
+        else if(Arrays.equals(rowOfX, this.board[1]))
+        {
+            return true;
+        }
+        else if(Arrays.equals(rowOfX, this.board[2]))
+        {
+            return true;
+        }
+        // TODO: Add in the rest of the winning conditions below.
+    }
+    public boolean oWon()
+    {
+        char[] rowOfO = {'O', 'O', 'O'};
+        if(Arrays.equals(rowOfO, this.board[0]))
+        {
+            return true;
+        }
+        else if(Arrays.equals(rowOfO, this.board[1]))
+        {
+            return true;
+        }
+        else if(Arrays.equals(rowOfO, this.board[2]))
+        {
+            return true;
+        }
+        // TODO: Add in the rest of the winning conditions below.
     }
 }
